c6f55e9b6378f0683620546c3d07a868f8c36140
YasserAet <yasseraitali@outlook.fr>
2026-01-06 14:54:27 +0100

    fix: correct base path to '/' for user site and revert index.html paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YasserAet.github.io',
+  base: '/',
   server: {
     port: 3000,
     open: true
